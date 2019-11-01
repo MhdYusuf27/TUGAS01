5892ac17bfa53570fed7f14960b315e4ee5b8319
Muhammad Yusuf <mhdyusuf27@gmaill.com>
2019-11-01 14:41:48 +0700

    Ambil Data

---
+++ gen/com/example/muhammadyusuf_163303030247_tugas1_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.muhammadyusuf_163303030247_tugas1_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
